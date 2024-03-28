28e0ee8e24bf60c86d5da438efc2d123d2613245
anjanathapa1 <anjanathapa731@gmail.com>
2024-03-28 12:19:21 +0545

    message

---
+++ assignment.js
@@ -0,0 +1,39 @@
+function Home(){
+    document.getElementById("Home").style.color="rgb(20,251,20)";
+    document.getElementById("Program").style.color="white";
+    document.getElementById("Plan").style.color="white";
+    document.getElementById("Blog").style.color="white";
+    document.getElementById("contact").style.color="white";
+}
+
+function Program(){
+    document.getElementById("Home").style.color="white";
+    document.getElementById("Program").style.color="rgb(20,251,20)";
+    document.getElementById("Plan").style.color="white";
+    document.getElementById("Blog").style.color="white";
+    document.getElementById("contact").style.color="white";
+}
+
+function Plan(){
+    document.getElementById("Home").style.color="white";
+    document.getElementById("Program").style.color="white";
+    document.getElementById("Plan").style.color="rgb(20,251,20)";
+    document.getElementById("Blog").style.color="white";
+    document.getElementById("contact").style.color="white";
+}
+
+function Blog(){
+    document.getElementById("Home").style.color="white";
+    document.getElementById("Program").style.color="white";
+    document.getElementById("Plan").style.color="white";
+    document.getElementById("Blog").style.color="rgb(20,251,20)";
+    document.getElementById("contact").style.color="white";
+}
+
+function contact(){
+    document.getElementById("Home").style.color="rgb(20,251,20)";
+    document.getElementById("Program").style.color="white";
+    document.getElementById("Plan").style.color="white";
+    document.getElementById("Blog").style.color="white";
+    document.getElementById("contact").style.color="rgb(20,251,20)";
+}
---
+++ st.py
@@ -0,0 +1,7 @@
+# print("enter two numbers")
+# a = eval(input("enter a numbers:"))
+# b = eval(input("enter a numbers:"))
+# c = a + b
+# print("sum is",c)
+
+print("hello\rsory")
